a7a7cede57d01fa0457d6b42d7ada72ccff89325
Jan Lukas Spah <janlukas.spaeh@physik.rwth-aachen.de>
2023-12-08 09:40:41 +0100

    Updating delphes cards (Run2 reduced info, pulling phaseII CMS 0PU)

--- MadGraph/cards/delphes_cards/pull_cards.sh
+++ MadGraph/cards/delphes_cards/pull_cards.sh
@@ -1,3 +1,4 @@
 mkdir -p common/
 # Alternative curl -O
-wget https://raw.githubusercontent.com/delphes/delphes/master/cards/delphes_card_CMS.tcl -P ./common/
+wget -nc https://raw.githubusercontent.com/delphes/delphes/master/cards/delphes_card_CMS.tcl -P ./common/
+wget -nc https://raw.githubusercontent.com/delphes/delphes/master/cards/CMS_PhaseII/CMS_PhaseII_0PU_v02.tcl -P ./common/
